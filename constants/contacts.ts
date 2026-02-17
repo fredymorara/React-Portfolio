0d297ac3a8f67d844e2207908c3e9b44330c94f1
Fredrick M. Morara <momanyifredm@gmail.com>
2026-02-17 14:19:54 +0300

    added welfound page

--- constants/contacts.ts
+++ constants/contacts.ts
@@ -1,17 +1,17 @@
 export const contacts = [
   {
-    id: 'linkedin',
-    title: 'LinkedIn',
-    icon: '/linkedin.svg',
+    id: "linkedin",
+    title: "LinkedIn",
+    icon: "/linkedin.svg",
     description: "Let's connect and explore opportunities together.",
-    path: 'https://www.linkedin.com/in/freddymorara/',
+    path: "https://www.linkedin.com/in/freddymorara/",
   },
   {
-    id: 'email',
-    title: 'Email',
-    icon: '/email.svg',
-    description: 'Feel free to reach out to me directly via email.',
-    path: 'mailto:momanyifredm@gmail.com',
+    id: "email",
+    title: "Email",
+    icon: "/email.svg",
+    description: "Feel free to reach out to me directly via email.",
+    path: "mailto:momanyifredm@gmail.com",
   },
 ];
 
@@ -23,27 +23,27 @@ export const socialLinks = [
     path: '',
   },*/
   {
-    id: 'github',
-    icon: '/github.svg',
+    id: "github",
+    icon: "/github.svg",
     alt: "Link to Fredrick's gitHub profile",
-    path: 'https://github.com/fredymorara',
+    path: "https://github.com/fredymorara",
   },
   {
-    id: 'linkedin',
-    icon: '/linkedin.svg',
+    id: "linkedin",
+    icon: "/linkedin.svg",
     alt: "Link to Fredrick's linkedin profile",
-    path: 'https://www.linkedin.com/in/freddymorara/',
+    path: "https://www.linkedin.com/in/freddymorara/",
   },
-  /*
-    id: 'wellfound',
-    icon: '/wellfound.svg',
-    alt: "Link to Fredrick's wellfound profile",
-    path: 'https://wellfound.com/u/',
-  */
   {
-    id: 'medium',
-    icon: '/medium.svg',
+    id: "wellfound",
+    icon: "/wellfound.svg",
+    alt: "Link to Fredrick's wellfound profile",
+    path: "https://wellfound.com/u/fredrick-momanyi-morara",
+  },
+  {
+    id: "medium",
+    icon: "/medium.svg",
     alt: "Link to Fredrick's medium profile",
-    path: 'https://medium.com/@freddymorara',
+    path: "https://medium.com/@freddymorara",
   },
 ];
