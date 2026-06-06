686af9069871819ec613ed879edb68f8ebdcac5c
Fredrick M. Morara <momanyifredm@gmail.com>
2026-06-07 02:39:30 +0300

    feat: add robots.txt and sitemap.xml configuration for SEO optimization

--- app/robots.ts
+++ app/robots.ts
@@ -4,7 +4,15 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
+      allow: [
+        '/',
+        '/#projects',
+        '/#certifications',
+        '/#stack',
+        '/#about',
+        '/#contact',
+        '/cv.pdf',
+      ],
     },
     sitemap: 'https://freddymorara.tech/sitemap.xml',
   };
