a824de4c9827fc86b8080606c26389f499372025
Fredrick M. Morara <momanyifredm@gmail.com>
2026-06-07 01:39:21 +0300

    feat: add app root layout with fonts and global metadata icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,20 +32,8 @@ export const metadata: Metadata = {
     'Software engineer',
   ],
   icons: {
-    icon: './favicon.ico',
-    apple: '/apple-touch-icon.png',
-    other: [
-      {
-        rel: 'icon',
-        url: '/favicon-32x32.png',
-        sizes: '32x32',
-      },
-      {
-        rel: 'icon',
-        url: '/favicon-16x16.png',
-        sizes: '16x16',
-      },
-    ],
+    icon: '/icon.svg',
+    apple: '/icon.svg',
   },
   openGraph: {
     type: 'website',
